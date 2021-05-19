fc2a2eb2c2a43dece4fccd10c6893ccbeeaee7bf
Khanh Tran <nkhanh.trn@gmail.com>
2021-05-19 11:50:02 +0700

    download images & convert to PDF

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import concurrent.futures
-from concurrent.futures import thread
+import img2pdf
 import requests
+import tempfile
 import threading
 import time
 from bs4 import BeautifulSoup
@@ -44,6 +45,23 @@ def get_images_url(url):
 
     return (chapter_name, image_urls)
 
+def download_images(folder_name, urls):
+    img_name = 1
+    with tempfile.TemporaryDirectory() as tmpDirName:
+        print(tmpDirName)
+        for url in urls:
+            ext = url.split('.')[-1]
+            fp, path = tempfile.mkstemp('.' + ext, str(img_name) + '__', tmpDirName)
+            with open(path, mode='wb') as f:
+                img_name += 1
+                response = requests.get(url)
+                f.write(response.content)
+
+        import glob
+        with open(folder_name + ".pdf","wb") as f:
+	        f.write(img2pdf.convert(glob.glob(tmpDirName + "/*." + ext)))
+    
+
 if __name__ == "__main__":
     domain = 'https://kissmanga.org'
     manga_url = '/manga/gd924067'
@@ -56,6 +74,9 @@ if __name__ == "__main__":
         for (name, urls) in executor.map(get_images_url, chapter_urls):
             chapters[name] = urls
 
-    print(chapters.keys())
+    for name in chapters:
+        download_images(name, chapters[name])
+        break
+
     duration = time.time() - start_time
     print(f"Downloaded in {duration} seconds")
