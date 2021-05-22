f348d69e8c1dbf0bd7ed8a70bfce77f3486b2a6f
Khanh Tran <nkhanh.trn@gmail.com>
2021-05-22 12:37:12 +0700

    add files to ZIP

--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
 import concurrent.futures
-import img2pdf
+#import img2pdf
+import os
 import requests
 import tempfile
 import threading
 import time
 from bs4 import BeautifulSoup
-
+from zipfile import ZIP_STORED, ZipFile
 
 thread_local = threading.local()
 
@@ -14,14 +15,15 @@ def get_session():
         thread_local.session = requests.Session()
     return thread_local.session
 
-def get_chapters_url(domain, manga_url):
+def get_chapters_url(manga_url):
     session = get_session()
     chapter_urls = []
 
-    with session.get(domain + manga_url) as response:
-        soup = BeautifulSoup(response.text, 'lxml')       
-        for chapter in soup.select('.listing a'):
-            chapter_urls.append(domain + chapter.get('href'))
+    with session.get(manga_url) as response:
+        soup = BeautifulSoup(response.text, "lxml")       
+        for chapter in soup.select(".listing a"):
+            domain = "https://kissmanga.org"
+            chapter_urls.append(domain + chapter.get("href"))
 
     return chapter_urls
 
@@ -29,19 +31,19 @@ def get_chapters_url(domain, manga_url):
 def get_images_url(url):
     session = get_session()
     image_urls = []
-    chapter_name = ''
+    chapter_name = ""
     
     with session.get(url) as response:
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(response.text, "lxml")
         
         # get chapter name
-        for chapter in soup.select('#selectEpisode option'):
-            if chapter.get('selected') is not None:
+        for chapter in soup.select("#selectEpisode option"):
+            if chapter.get("selected") is not None:
                 chapter_name = chapter.text.strip()
                 break
         
-        for image in soup.select('#centerDivVideo img'):
-            image_urls.append(image.get('src'))
+        for image in soup.select("#centerDivVideo img"):
+            image_urls.append(image.get("src"))
 
     return (chapter_name, image_urls)
 
@@ -51,13 +53,13 @@ def download_images(folder_name, urls):
     def download_img(url):
         global my_lock
         global img_count
-        img_name = ''
+        img_name = ""
         with my_lock:
-            img_name = str(img_count).zfill(4)
+            img_name = str(img_count).zfill(6)
             img_count += 1
-        fp, path = tempfile.mkstemp('.jpg', img_name + '__', tmpDirName)
+        fp, path = tempfile.mkstemp(".jpg", img_name + "__", tmpDirName)
 
-        with open(path, mode='wb') as f:
+        with open(path, mode="wb") as f:
             response = requests.get(url)
             f.write(response.content)
         
@@ -66,31 +68,36 @@ def download_images(folder_name, urls):
     with tempfile.TemporaryDirectory() as tmpDirName:
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             executor.map(download_img, urls) 
-        
-        import glob
-        with open(folder_name + ".pdf","wb") as f:
-            f.write(img2pdf.convert(sorted(glob.glob(tmpDirName + "/*.jpg"))))
 
-        duration = time.time() - start_time
+        import glob
+        #with open(folder_name + ".pdf","wb") as f:
+        #    f.write(img2pdf.convert(sorted(glob.glob(tmpDirName + "/*.jpg"))))
+        with ZipFile(f"{folder_name}.zip", "w") as zip_file:
+            for file in sorted(glob.glob(tmpDirName + "/*.jpg")):
+                zip_file.write(file, os.path.basename(file))
+
+        duration = round(time.time() - start_time, 2)
         print(f"Finished download {folder_name} in {duration}s.")
 
 
 if __name__ == "__main__":
-    domain = 'https://kissmanga.org'
-    manga_url = '/manga/gd924067'
+    manga_url = input("Enter the manga URL: ")
     start_time = time.time()
     img_count = 1
     my_lock = threading.Lock()
 
     chapter_urls = []
-    chapter_urls = get_chapters_url(domain, manga_url)
+    print("Getting list of chapters...")
+    chapter_urls = get_chapters_url(manga_url)
     chapters = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         for (name, urls) in executor.map(get_images_url, chapter_urls):
             chapters[name] = urls
+    print("DONE")
 
-    for name in chapters:
+    print("Downloading chapters...")
+    for name in reversed(list(chapters.keys())):
         download_images(name, chapters[name])
-
-    duration = time.time() - start_time
-    print(f"Finished in {duration} seconds")
+        
+    duration = round(time.time() - start_time, 2)
+    print(f"DONE in {duration} seconds")
