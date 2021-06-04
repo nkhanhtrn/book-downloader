a8edf82db6234437756055f8ee771546ce6690bb
Khanh Tran <nkhanh.trn@gmail.com>
2021-06-04 09:43:16 +0700

    print menu once

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
-echo "Choose your option: "
-echo "  1) Download Manga"
-echo "  2) Update Mange Software"
-echo "  3) Others"
-echo "  4) QUIT"
+PS3="Select an option and press Enter: "
+options=("Download Manga" "Update Software" "Others" "QUIT")
 
-options=(1 2 3 4)
 select opt in "${options[@]}"
 do
-  case $opt in
+  case $REPLY in
     1)
       cd ~/Downloads/Manga && python3 parser.py
       ;;
